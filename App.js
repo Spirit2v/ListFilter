93616c27169311cd20a9fe769deade6af5ecf28a
Suyash Srivastava <55198615+Spirit2v@users.noreply.github.com>
2021-09-13 18:17:34 +0530

    l

--- App.js
+++ App.js
@@ -6,50 +6,29 @@ import {
   FlatList,
   StatusBar,
   TouchableOpacity,
+  StyleSheet,
 } from 'react-native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 export default function App() {
-  const change=(e)=>{
-    setData(e)
-  }
-  const add =()=>{
-    setarray([...array,{name:data,id:Math.random()}])
-  }
+  const change = e => {
+    setData(e);
+  };
+  const add = () => {
+    setarray([...array, {name: data, id: Math.random()}]);
+  };
   const renderItem = ({item}) => (
-  //   <View>
-  //     {data ? (
-  //       // <View>{data == item.name ? <Text>{data}</Text> : null}</View>
-  //       item.filter((val)=>{
-  //         val.name.toLowerCase().includes(data.toLowerCase())
-  //       }).map((val,key) => {
-
-  // <Text>{val.name}</Text>
-
-  //       })
-  //     ) : (
-        <View
-          style={{
-            borderColor: 'grey',
-            marginVertical: 6,
-            padding: 12,
-            borderBottomWidth: 1,
-          }}>
-          <Text style={{fontWeight: 'bold',fontSize:
-         18 }}>{data ?
+    <View style={styles.render}>
+      <Text style={{fontWeight: 'bold', fontSize: 18}}>
+        {data ? (
           <>
-      <Text
-      style={{color:'yellow'}}
-      >
-      </Text>  
-          <Text
-          style={{color:'black'}}
-          >{ item.name}
-          </Text></>   :
-        item.name
-        }</Text>
-        </View>
-    //   )}
-    // </View>
+            <Text style={{color: 'yellow'}}></Text>
+            <Text style={{color: 'black'}}>{item.name}</Text>
+          </>
+        ) : (
+          item.name
+        )}
+      </Text>
+    </View>
   );
   const [array, setarray] = useState([
     {id: 1, name: 'Milk'},
@@ -57,70 +36,67 @@ export default function App() {
     {id: 3, name: 'Oranges'},
     {id: 4, name: 'Bread'},
   ]);
-  const arr = [
-    {id: 1, name: 'Milk'},
-    {id: 2, name: 'Coffee'},
-    {id: 3, name: 'Oranges'},
-    {id: 4, name: 'Bread'},
-  ];
+
   const [data, setData] = useState();
-const newr=()=>{
-data ? array
-:
-filteredData
-}
+  const newr = () => {
+    data ? array : filteredData;
+  };
   const filteredData = data
-  ? array.filter(x =>
-      x.name.toLowerCase().includes(data.toLowerCase())
-    )
-  : array;
+    ? array.filter(x => x.name.toLowerCase().includes(data.toLowerCase()))
+    : array;
+
   return (
     <>
       <StatusBar animated={true} backgroundColor="#61dafb" />
-      <View style={{flex: 1, paddingHorizontal: 20, paddingVertical: 9}}>
-        <View
-          style={{
-            flexDirection: 'row',
 
-            borderBottomWidth: 1,
-          }}>
+      <View style={styles.box}>
+        <View style={styles.header}>
           <TextInput
             placeholder="Search"
-            onChangeText={change}
-            style={{
-              borderColor: 'black',
-              borderWidth: 1,
-              width: '85%',
-              padding: 12,
-              height: 52,
-              marginVertical: 6,
-            }}
+            onChangeText={text => setData(text)}
+            style={styles.input}
           />
 
-          {/* <Text>{data}</Text> */}
-          <View
-            style={{
-              height: 71,
-              backgroundColor: 'black',
-              width: 1.3,
-              marginHorizontal: 10,
-            }}></View>
+          <View style={styles.line}></View>
+
           <View style={{marginVertical: 12}}>
             <TouchableOpacity onPress={add}>
-            {/* <ion-icon name="add-outline"></ion-icon> */}
-            {/* <Ionicons name='pencil-outline' size={18} color='black'/> */}
-            <Ionicons name="add-outline" size={42} color='black' />
+              <Ionicons name="add-outline" size={42} color="black" />
             </TouchableOpacity>
           </View>
         </View>
+
         <View>
-          {/* <Text>{data}</Text>
-           */}
-      
           <FlatList data={filteredData} renderItem={renderItem} />
         </View>
-        <Text></Text>
       </View>
     </>
   );
 }
+const styles = StyleSheet.create({
+  box: {flex: 1, paddingHorizontal: 20, paddingVertical: 9},
+  header: {
+    flexDirection: 'row',
+    borderBottomWidth: 1,
+  },
+  input: {
+    borderColor: 'black',
+    borderWidth: 1,
+    width: '85%',
+    padding: 12,
+    height: 52,
+    marginVertical: 6,
+  },
+  render: {
+    borderColor: 'grey',
+    marginVertical: 6,
+    padding: 12,
+    borderBottomWidth: 1,
+  },
+  line: {
+    height: 71,
+    backgroundColor: 'black',
+    width: 1.3,
+    marginHorizontal: 10,
+  },
+});
